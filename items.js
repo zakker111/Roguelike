e677dcbff3e77e7181e1d26f61803539ade15fab
zakker111 <zakker690@gmail.com>
2025-09-17 23:00:03 +0000

    feat: add new "swich_blade" weapon to items list

--- items.js
+++ items.js
@@ -33,6 +33,12 @@ Notes:
       atkRange: { 1: [0.5, 2.4], 2: [1.2, 3.4], 3: [2.2, 4.0] },
       atkBonus: { 1: [0.0, 0.3], 2: [0.1, 0.5], 3: [0.2, 0.6] } },
 
+axe: { key: "swich_blade", slot: "hand", twoHanded: false,
+      weight: 0.25,
+      name: (mat) => `${mat} swich_blade`,
+      atkRange: { 1: [0.6, 1.4], 2: [1.3, 3.4], 3: [2.2, 4.0] },
+      atkBonus: { 1: [1.0, 0.3], 2: [0.1, 0.5], 3: [0.2, 0.6] } },
+
       
     gasoline_thrower: { key: "gasoline_thrower", slot: "hand", twoHanded: false,
       weight: 0.21,
